b6cbbc177561e3f00a5587fcbdad89ead901d016
rgurko <gurko.roman@gmail.com>
2020-10-28 22:48:37 +0300

    First task

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log(1);
+"use strict"
+const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+      
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
